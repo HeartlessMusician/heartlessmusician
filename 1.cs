82641ba84a2622aa2b496d51bf71c153b8f3f1e1
HeartlessMusician <102092470+HeartlessMusician@users.noreply.github.com>
2022-03-24 03:05:05 +0300

    Add files via upload

---
+++ 1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp4
+{
+    class Symbol 
+    {
+        public EventHandler <char> OnKeyPressed;
+        public void Run()
+        {
+            Console.WriteLine("Введите необходимый символ");
+            char ch = (char)Console.Read();
+            while (ch !='c')
+            {
+                OnKeyPressed(this, ch);
+                ch = (char)Console.Read();
+                  }
+            }
+        }
+
+}
+
+
+
+
---
+++ 2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Symbol1
+    {
+        static void Main(string[] args)
+        {
+            var charWriter = new Symbol();
+            charWriter.OnKeyPressed += (sender, ch) => Console.WriteLine(ch);
+            charWriter.Run();
+        }
+    }
+}
